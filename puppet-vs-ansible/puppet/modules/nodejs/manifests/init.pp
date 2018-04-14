dfd8dc0017c04679384d14535463c78335e7675a
sparsick <sparsick@web.de>
2018-04-14 15:37:06 +0200

    update nodejs installation

--- puppet-vs-ansible/puppet/modules/nodejs/manifests/init.pp
+++ puppet-vs-ansible/puppet/modules/nodejs/manifests/init.pp
@@ -11,27 +11,24 @@
 # Sample Usage:
 #
 class nodejs {
-  
-  class { 'apt':  
+
+  class { 'apt':
   }
-  
-  exec { 'apt-get-update':  
+
+  exec { 'apt-get-update':
     command     => '/usr/bin/apt-get update',
   }
-  
+
   package {'software-properties-common' :
     ensure=> installed,
     require => Exec['apt-get-update'],
   }
-  
+
   apt::ppa {'ppa:chris-lea/node.js' :}
-  
+
   package { 'nodejs' :
     ensure => installed,
     require => Apt::Ppa ['ppa:chris-lea/node.js'],
-    
-  }
-  
-  
 
+  }
 }
