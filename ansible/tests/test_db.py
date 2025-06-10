f182aef3ac2f8d8fe3fd6ac33ca21c1e5a2efa4a
Sandra Parsick <sandra+gh@parsick.dev>
2025-06-10 19:56:13 +0200

    chore: update tests

--- ansible/tests/test_db.py
+++ ansible/tests/test_db.py
@@ -1,12 +1,12 @@
-def test_mysql_is_installed(host):
-    mysql = host.package("mysql-server")
-    assert mysql.is_installed
+def test_mariadb_is_installed(host):
+    mariadb = host.package("mariadb-server")
+    assert mariadb.is_installed
 
-def test_mysql_service_is_running(host):
-    mysql = host.service("mysql")
-    assert mysql.is_enabled
-    assert mysql.is_running
+def test_mariadb_service_is_running(host):
+    mariadb = host.service("mariadb")
+    assert mariadb.is_enabled
+    assert mariadb.is_running
 
-def test_mysql_config_parameter_exists(host):
-    mysql_conf = host.file("/etc/mysql/mysql.conf.d/mysqld.cnf")
-    assert mysql_conf.contains("bind-address = 0.0.0.0")
+def test_mariadb_config_parameter_exists(host):
+    mariadb_conf = host.file("/etc/mysql/mariadb.conf.d/50-server.cnf")
+    assert mariadb_conf.contains("bind-address = 0.0.0.0")
